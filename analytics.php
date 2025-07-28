427508073a6fa79e0c951361de48b828032e06ff
mojossj <116288588+mojossj@users.noreply.github.com>
2025-07-28 12:51:15 +0200

    Update analytics.php

--- analytics.php
+++ analytics.php
@@ -1,12 +1,13 @@
 <?php
 // INSERISCI QUI IL CODICE DI GOOGLE ANALYTICS
 // Esempio: 
-// <!-- Google tag (gtag.js) -->
-// <script async src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID"></script>
-// <script>
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag(){dataLayer.push(arguments);}
-//   gtag('js', new Date());
-//   gtag('config', 'GA_MEASUREMENT_ID');
-// </script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-4GR7X9L8MN"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4GR7X9L8MN');
+</script>
 ?>
